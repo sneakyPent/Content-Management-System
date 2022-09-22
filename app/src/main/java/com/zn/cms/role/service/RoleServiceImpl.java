3487a5be06d13055d49b3c5550f7f00e77a4f978
sneakyPent <zaharioudakis@yahoo.com>
2022-09-23 00:19:24 +0300

    unfinished implementation of createRoleIfNotFound in RoleServiceImpl.java - missing saving role with the found permission

--- app/src/main/java/com/zn/cms/role/service/RoleServiceImpl.java
+++ app/src/main/java/com/zn/cms/role/service/RoleServiceImpl.java
@@ -1,5 +1,8 @@
 package com.zn.cms.role.service;
 
+
+import com.zn.cms.permission.model.Permission;
+import com.zn.cms.permission.repository.PermissionRepository;
 import com.zn.cms.role.dto.RoleDTO;
 import com.zn.cms.role.model.Role;
 import com.zn.cms.role.repository.RoleRepository;
@@ -7,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +19,7 @@ import java.util.Optional;
 public class RoleServiceImpl implements IRoleService {
 
     private final RoleRepository roleRepository;
+    private  final PermissionRepository permissionRepository;
 
     @PostConstruct
     public void init() {
@@ -30,8 +36,12 @@ public class RoleServiceImpl implements IRoleService {
 
     private Optional<Role> createRoleIfNotFound(String name) {
         Optional<Role> role = roleRepository.findByName(name);
+        Optional<Permission> permission = permissionRepository.findByName("READ_PRIVILEGE");
+        List<Permission> permissions = new ArrayList<Permission>();
         if (!role.isPresent()) {
-            System.out.println("----------> Role not found");
+            roleRepository.save(
+                    Role.builder()
+                            .name(name).build());
         }
         return role;
     }
