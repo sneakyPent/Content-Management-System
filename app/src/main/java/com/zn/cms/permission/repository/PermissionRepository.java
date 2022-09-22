f05ad4befe06255c325ff6ef7bd3b16e51093727
zn <zaharioudakis@yahoo.com>
2022-09-22 17:31:47 +0300

    added new method findByName for Permission services in PermissionRepository.java

--- app/src/main/java/com/zn/cms/permission/repository/PermissionRepository.java
+++ app/src/main/java/com/zn/cms/permission/repository/PermissionRepository.java
@@ -3,7 +3,11 @@ package com.zn.cms.permission.repository;
 import com.zn.cms.permission.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+
+    public Optional<Permission> findByName(String permissionName);
+
 }
