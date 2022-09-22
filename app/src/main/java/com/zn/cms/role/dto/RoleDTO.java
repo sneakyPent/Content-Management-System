a1f36149f3c4a5511a7018da6265df6661dde70f
zn <zaharioudakis@yahoo.com>
2022-09-22 17:27:54 +0300

    implement RoleDTO

--- app/src/main/java/com/zn/cms/role/dto/RoleDTO.java
+++ app/src/main/java/com/zn/cms/role/dto/RoleDTO.java
@@ -1,6 +1,10 @@
 package com.zn.cms.role.dto;
 
+import com.zn.cms.permission.model.Permission;
+import com.zn.cms.role.model.Role;
 import lombok.*;
+
+import java.util.Collection;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -9,4 +13,10 @@ import lombok.*;
 public class RoleDTO {
 
     private String name;
+    private Collection<Permission> permissions;
+
+    public RoleDTO(Role role) {
+        this.name = role.getName();
+        this.permissions = this.getPermissions();
+    }
 }
