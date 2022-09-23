34a82edf6cc77e637f58faa5f9f4d79339d09cb2
zn <zaharioudakis@yahoo.com>
2022-09-23 16:58:21 +0300

    create findAll method for responding request in RoleService

--- app/src/main/java/com/zn/cms/role/service/RoleService.java
+++ app/src/main/java/com/zn/cms/role/service/RoleService.java
@@ -1,11 +1,15 @@
 package com.zn.cms.role.service;
 
 import com.zn.cms.role.dto.RoleDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RoleService {
+
+    Page<RoleDTO> findAll(Pageable pageable);
 
     Optional<RoleDTO> findByName(String roleName);
 
