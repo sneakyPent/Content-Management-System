915867a8b3580eadd7748e6f1558349e614e713d
zn <zaharioudakis@yahoo.com>
2022-09-23 15:07:14 +0300

    added findAllByNameIn method in PermissionRepository

--- app/src/main/java/com/zn/cms/permission/repository/PermissionRepository.java
+++ app/src/main/java/com/zn/cms/permission/repository/PermissionRepository.java
@@ -3,11 +3,15 @@ package com.zn.cms.permission.repository;
 import com.zn.cms.permission.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
-    public Optional<Permission> findByName(String permissionName);
+    Optional<Permission> findByName(String permissionName);
+
+    List<Permission> findAllByNameIn(List<String> permissionNames);
 
 }
