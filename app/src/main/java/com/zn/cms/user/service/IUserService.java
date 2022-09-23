de6f36558b35ece132acaa723e1672dc5b69e167
zn <zaharioudakis@yahoo.com>
2022-09-23 15:11:01 +0300

    added createUserIfNotFound IUserService

--- app/src/main/java/com/zn/cms/user/service/IUserService.java
+++ app/src/main/java/com/zn/cms/user/service/IUserService.java
@@ -2,10 +2,13 @@ package com.zn.cms.user.service;
 
 import com.zn.cms.user.dto.UserDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IUserService {
 
     Optional<UserDTO> findByUsername(String username);
+
+    Optional<UserDTO> createUserIfNotFound(String userName, String password, List<String> roleNames);
 
 }
