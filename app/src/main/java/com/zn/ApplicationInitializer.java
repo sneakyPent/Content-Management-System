ccc482530af02908adb2261c474bc6fa2703220a
zn <zaharioudakis@yahoo.com>
2022-09-23 15:01:42 +0300

    implement CommandLineRunner for application initialization

--- app/src/main/java/com/zn/ApplicationInitializer.java
+++ app/src/main/java/com/zn/ApplicationInitializer.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
