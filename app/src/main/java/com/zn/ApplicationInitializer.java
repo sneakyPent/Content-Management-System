047cd112c8636adf98157de3966a9b91e268bd4b
zn <zaharioudakis@yahoo.com>
2022-09-23 15:01:32 +0300

    implement CommandLineRunner for application initialization

---
+++ app/src/main/java/com/zn/ApplicationInitializer.java
@@ -0,0 +1,37 @@
+package com.zn;
+
+import com.zn.cms.permission.service.IPermissionService;
+import com.zn.cms.role.service.IRoleService;
+import com.zn.cms.user.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class ApplicationInitializer implements CommandLineRunner {
+
+    private final IPermissionService permissionService;
+    private final IRoleService roleService;
+    private final IUserService userService;
+
+    @Value("${application.security.user.admin}")
+    private String adminUser;
+
+    @Value("${application.security.user.password}")
+    private String password;
+
+    @Override
+    public void run(String... args) {
+        permissionService.createPermissionIfNotFound("READ_PRIVILEGE");
+        roleService.createRoleIfNotFound("ROLE_ADMIN", Collections.singletonList("READ_PRIVILEGE"));
+        userService.createUserIfNotFound(adminUser, password, Collections.singletonList("ROLE_ADMIN"));
+
+    }
+}
