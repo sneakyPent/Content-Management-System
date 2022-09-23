c75c950dd605e6e90f9851d472c16c2eb541a819
zn <zaharioudakis@yahoo.com>
2022-09-23 15:11:26 +0300

    added findAllByNameIn and createPermissionIfNotFound methods in IPermissionService

--- app/src/main/java/com/zn/cms/permission/service/IPermissionService.java
+++ app/src/main/java/com/zn/cms/permission/service/IPermissionService.java
@@ -2,9 +2,14 @@ package com.zn.cms.permission.service;
 
 import com.zn.cms.permission.dto.PermissionDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IPermissionService {
 
     Optional<PermissionDTO> findByName(String name);
+
+    List<PermissionDTO> findAllByNameIn(List<String> permissionNames);
+
+    Optional<PermissionDTO> createPermissionIfNotFound(String name);
 }
