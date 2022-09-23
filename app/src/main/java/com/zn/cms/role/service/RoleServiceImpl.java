a8f0bb58f028c31e3b75068bf8621607df583ed0
zn <zaharioudakis@yahoo.com>
2022-09-23 15:06:04 +0300

    implement findAllByNameIn method in RoleServiceImpl

--- app/src/main/java/com/zn/cms/role/service/RoleServiceImpl.java
+++ app/src/main/java/com/zn/cms/role/service/RoleServiceImpl.java
@@ -1,31 +1,25 @@
 package com.zn.cms.role.service;
 
-
 import com.zn.cms.permission.model.Permission;
 import com.zn.cms.permission.repository.PermissionRepository;
 import com.zn.cms.role.dto.RoleDTO;
+import com.zn.cms.role.mapper.RoleMapper;
 import com.zn.cms.role.model.Role;
 import com.zn.cms.role.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements IRoleService {
 
     private final RoleRepository roleRepository;
-    private  final PermissionRepository permissionRepository;
-
-    @PostConstruct
-    public void init() {
-        String name = "ROLE_ADMIN";
-        createRoleIfNotFound(name);
-    }
+    private final RoleMapper roleMapper;
+    private final PermissionRepository permissionRepository;
 
 
     @Override
@@ -33,6 +27,10 @@ public class RoleServiceImpl implements IRoleService {
         return roleRepository.findByName(roleName).map(RoleDTO::new);
     }
 
+    @Override
+    public List<RoleDTO> findAllByNameIn(List<String> roleNames) {
+        return roleRepository.findAllByNameIn(roleNames).stream().map(roleMapper::roleToRoleDTO).collect(Collectors.toList());
+    }
 
     private Optional<Role> createRoleIfNotFound(String name) {
         Optional<Role> role = roleRepository.findByName(name);
