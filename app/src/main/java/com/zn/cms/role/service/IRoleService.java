21aba7a500649ab56a82279170589e86c9ea9476
zn <zaharioudakis@yahoo.com>
2022-09-23 15:04:05 +0300

    add two new methods in IRoleService findAllByNameIn and createRoleIfNotFound

--- app/src/main/java/com/zn/cms/role/service/IRoleService.java
+++ app/src/main/java/com/zn/cms/role/service/IRoleService.java
@@ -2,10 +2,15 @@ package com.zn.cms.role.service;
 
 import com.zn.cms.role.dto.RoleDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IRoleService {
 
     Optional<RoleDTO> findByName(String roleName);
+
+    List<RoleDTO> findAllByNameIn(List<String> roleNames);
+
+    Optional<RoleDTO> createRoleIfNotFound(String name, List<String> permissionNames);
 
 }
