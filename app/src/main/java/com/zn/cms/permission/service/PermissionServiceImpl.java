8764e4ec5ae4887abc54ee5197a99c59e5af904a
zn <zaharioudakis@yahoo.com>
2022-09-23 15:08:40 +0300

    implement findAllByNameIn method in PermissionServiceImpl and update createPermissionIfNotFound method to return  Optional<PermissionDTO>

--- app/src/main/java/com/zn/cms/permission/service/PermissionServiceImpl.java
+++ app/src/main/java/com/zn/cms/permission/service/PermissionServiceImpl.java
@@ -1,32 +1,35 @@
 package com.zn.cms.permission.service;
 
 import com.zn.cms.permission.dto.PermissionDTO;
+import com.zn.cms.permission.mapper.PermissionMapper;
 import com.zn.cms.permission.model.Permission;
 import com.zn.cms.permission.repository.PermissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PermissionServiceImpl implements IPermissionService {
     private final PermissionRepository permissionRepository;
+    private final PermissionMapper permissionMapper;
 
-    @PostConstruct
-    public void init() {
-        String permission = "READ_PRIVILEGE";
-        createPermissionIfNotFound(permission);
-    }
 
     @Override
     public Optional<PermissionDTO> findByName(String name) {
         return permissionRepository.findByName(name).map(PermissionDTO::new);
     }
 
-    public Optional<Permission> createPermissionIfNotFound(String name) {
-        Optional<Permission> permission = permissionRepository.findByName(name);
+    @Override
+    public List<PermissionDTO> findAllByNameIn(List<String> permissionNames) {
+        return permissionRepository.findAllByNameIn(permissionNames).stream().map(permissionMapper::permissionToPermissionDTO).collect(Collectors.toList());
+    }
+
+    public Optional<PermissionDTO> createPermissionIfNotFound(String name) {
+        Optional<PermissionDTO> permission = permissionRepository.findByName(name).map(PermissionDTO::new);
         if (!permission.isPresent()) {
             permissionRepository.save(
                     Permission.builder()
