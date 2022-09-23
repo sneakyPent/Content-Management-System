bd92bbfe6c2a5db7ddbf9f1088546f15967160ec
zn <zaharioudakis@yahoo.com>
2022-09-23 15:05:02 +0300

    added findAllByNameIn method in RoleRepository

--- app/src/main/java/com/zn/cms/role/repository/RoleRepository.java
+++ app/src/main/java/com/zn/cms/role/repository/RoleRepository.java
@@ -3,10 +3,14 @@ package com.zn.cms.role.repository;
 import com.zn.cms.role.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByName(String roleName);
+
+    List<Role> findAllByNameIn(List<String> roleNames);
 }
