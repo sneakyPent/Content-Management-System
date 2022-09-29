86ce2ae7c0598b57838f43842fe508cbb2af7481
zn <zaharioudakis@yahoo.com>
2022-09-29 12:27:33 +0300

    create basic request methods in user userController

--- app/src/main/java/com/zn/cms/user/controller/UserController.java
+++ app/src/main/java/com/zn/cms/user/controller/UserController.java
@@ -1,15 +1,36 @@
 package com.zn.cms.user.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.zn.cms.utils.Router.USER;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(USER)
 public class UserController {
+
     @GetMapping("/")
-    public ResponseEntity<String> greetings(){
-        return ResponseEntity.ok("Hello Spring boot!");
+    public ResponseEntity<String> findAll(){
+        return ResponseEntity.ok("Getting all users!");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> findUser(@PathVariable String id){
+        return ResponseEntity.ok("find user with id=" + id);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> create(){
+        return ResponseEntity.ok("created user!");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> modifyUser(@PathVariable String id){
+        return ResponseEntity.ok("modify user with id=" + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id){
+        return ResponseEntity.ok("delete user with id=" + id);
     }
 }
