2120f4d4b5df27361d698cf4ed102b668a214d56
zn <zaharioudakis@yahoo.com>
2022-09-29 21:42:42 +0300

    create getAuthorities method to find permissions through user's role and add them to spring security user authorities

--- app/src/main/java/com/zn/cms/user/service/CmsUserDetailServiceImpl.java
+++ app/src/main/java/com/zn/cms/user/service/CmsUserDetailServiceImpl.java
@@ -1,17 +1,26 @@
 package com.zn.cms.user.service;
 
+import com.zn.cms.permission.dto.PermissionDTO;
 import com.zn.cms.role.dto.RoleDTO;
+import com.zn.cms.user.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CmsUserDetailServiceImpl implements UserDetailsService {
 
     private final UserService userService;
@@ -20,10 +29,28 @@ public class CmsUserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userService.findByUsername(username).map(
+        UserDetails usrDet = userService.findByUsername(username).map(
                 user -> new org.springframework.security.core.userdetails.User(user.getUsername(),
                         user.getPassword(),
-                        user.getRoles().stream().map(RoleDTO::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toList()))
+                        getAuthorities(user))
         ).orElseThrow(() -> new UsernameNotFoundException("Email " + username + "was not found"));
+        log.info("UserDetails {}", usrDet);
+        return usrDet;
+    }
+
+    private Set<GrantedAuthority> getAuthorities(UserDTO userDTO){
+        List<String> authorityNames = new ArrayList<>();
+        for (RoleDTO role : userDTO.getRoles()) {
+            for (PermissionDTO permission : role.getPermissions()) {
+                authorityNames.add(permission.getName());
+                for (PermissionDTO dependsOnPermission : permission.getDependsOnPermissions()) {
+                    authorityNames.add(dependsOnPermission.getName());
+                }
+            }
+        }
+        Set<GrantedAuthority> auths =  authorityNames.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+        log.info("authorityNames {}", auths);
+        return auths;
+
     }
 }
