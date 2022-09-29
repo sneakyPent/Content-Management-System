22152c364c204a1f9486a84e740344018f118142
zn <zaharioudakis@yahoo.com>
2022-09-29 17:08:20 +0300

    added getPermission into PermissionController to get info about specific permission

--- app/src/main/java/com/zn/cms/permission/controller/PermissionController.java
+++ app/src/main/java/com/zn/cms/permission/controller/PermissionController.java
@@ -2,16 +2,18 @@ package com.zn.cms.permission.controller;
 
 import com.zn.cms.permission.dto.PermissionDTO;
 import com.zn.cms.permission.service.PermissionService;
-import com.zn.cms.permission.service.PermissionServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.query.JpaEntityGraph;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.Optional;
 
 import static com.zn.cms.utils.Router.PERMISSION;
 
@@ -27,6 +29,13 @@ public class PermissionController {
     public ResponseEntity<Page<PermissionDTO>> getAllPermissions(Pageable pageable) {
 
         return ResponseEntity.ok(permissionService.findAll(pageable));
+    }
+
+    @GetMapping("/{name}")
+    @PreAuthorize("hasAuthority('READ_PERMISSION')")
+    public ResponseEntity<Optional<PermissionDTO>> getPermission(@PathVariable String name) {
+
+        return ResponseEntity.ok(permissionService.findByName(name));
     }
 
 }
