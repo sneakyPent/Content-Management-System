17c0ab7547ce4853da2423529de94e1bd32ec59b
zn <zaharioudakis@yahoo.com>
2022-09-30 11:25:13 +0300

    update UserController.java for Create, Update, DELETE

--- app/src/main/java/com/zn/cms/user/controller/UserController.java
+++ app/src/main/java/com/zn/cms/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.zn.cms.user.controller;
 
+import com.zn.cms.role.dto.RoleDTO;
 import com.zn.cms.user.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -7,8 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.zn.cms.user.service.UserService;
-
 import org.springframework.data.domain.Pageable;
+
+import java.util.stream.Collectors;
 
 import static com.zn.cms.utils.Router.USER;
 
@@ -18,33 +20,38 @@ import static com.zn.cms.utils.Router.USER;
 public class UserController {
 
     private final UserService userService;
+
     @GetMapping("/")
     @PreAuthorize("hasAuthority('READ_USER')")
-    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable){
+    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
         return ResponseEntity.ok(userService.findAll(pageable));
     }
 
     @GetMapping("/{username}")
     @PreAuthorize("#username == authentication.name or hasAuthority('READ_USER')")
-    public ResponseEntity<String> findUser(@PathVariable String username){
-        return ResponseEntity.ok("find user with id=" + username);
+    public ResponseEntity<UserDTO> findUser(@PathVariable String username) {
+        return userService.findByUsername(username).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
     @PostMapping("/")
     @PreAuthorize("hasAuthority('CREATE_USER')")
-    public ResponseEntity<String> create(){
-        return ResponseEntity.ok("created user!");
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return userService.createUserIfNotFound(userDTO.getEmail(), userDTO.getFirstName(),
+                userDTO.getLastName(), userDTO.getUsername(), userDTO.getPassword(),
+                userDTO.getRoles().stream().map(RoleDTO::getName).collect(Collectors.toList())).
+                map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasAuthority('UPDATE_USER')")
-    public ResponseEntity<String> modifyUser(@PathVariable String id){
-        return ResponseEntity.ok("modify user with id=" + id);
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return userService.updateUser(id, userDTO).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('DELETE_USER')")
-    public ResponseEntity<String> deleteUser(@PathVariable String id){
-        return ResponseEntity.ok("delete user with id=" + id);
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
