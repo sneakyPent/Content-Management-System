48c27f982a388f6ce3647a2223435edc633a0995
zn <zaharioudakis@yahoo.com>
2022-10-10 17:30:21 +0300

    create generate password method in ApplicationSecurity

--- app/src/main/java/com/zn/cms/configuration/ApplicationSecurity.java
+++ app/src/main/java/com/zn/cms/configuration/ApplicationSecurity.java
@@ -1,6 +1,10 @@
 package com.zn.cms.configuration;
 
 import com.zn.cms.user.service.CmsUserDetailServiceImpl;
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +18,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.beans.TypeMismatchException.ERROR_CODE;
 
 @Configuration
 @EnableWebSecurity
@@ -73,6 +79,36 @@ public class ApplicationSecurity {
         http.csrf().disable();
 
         return http.build();
+    }
+    @Bean
+    public String generatePassword() {
+        PasswordGenerator gen = new PasswordGenerator();
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            public String getCharacters() {
+                return "!@#$%^&*()_+";
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
+
+        return gen.generatePassword(10, splCharRule, lowerCaseRule,
+                upperCaseRule, digitRule);
     }
 
 
