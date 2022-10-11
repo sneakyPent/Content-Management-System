583e8637be7025455843da5fd6451d51c63fb414
zn <zaharioudakis@yahoo.com>
2022-10-11 16:24:40 +0300

    create method to read init json files and init Users Roles and Permissions

--- app/src/main/java/com/zn/ApplicationInitializer.java
+++ app/src/main/java/com/zn/ApplicationInitializer.java
@@ -1,15 +1,24 @@
 package com.zn;
 
+import com.zn.cms.permission.model.Permission;
 import com.zn.cms.permission.service.PermissionService;
 import com.zn.cms.role.service.RoleService;
+import com.zn.cms.user.dto.UserDTO;
 import com.zn.cms.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 @RequiredArgsConstructor
 @Component
@@ -31,27 +40,94 @@ public class ApplicationInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        permissionService.createPermissionIfNotFound("READ_PERMISSION", Arrays.asList());
-        permissionService.createPermissionIfNotFound("READ_ROLE", Arrays.asList());
-        permissionService.createPermissionIfNotFound("CREATE_ROLE", Arrays.asList("READ_ROLE"));
-        permissionService.createPermissionIfNotFound("DELETE_ROLE", Arrays.asList("READ_ROLE"));
-        permissionService.createPermissionIfNotFound("UPDATE_ROLE", Arrays.asList("READ_ROLE"));
-        permissionService.createPermissionIfNotFound("READ_USER", Arrays.asList());
-        permissionService.createPermissionIfNotFound("CREATE_USER", Arrays.asList("READ_USER"));
-        permissionService.createPermissionIfNotFound("UPDATE_USER", Arrays.asList("READ_USER"));
-        permissionService.createPermissionIfNotFound("DELETE_USER", Arrays.asList("READ_USER"));
+        initPermissions();
+        initRoles();
+        initUsers();
 
-        roleService.createRoleIfNotFound("ROLE_ADMIN", Arrays.asList("READ_PERMISSION","CREATE_ROLE","DELETE_ROLE",
-                "UPDATE_ROLE","CREATE_USER","UPDATE_USER","DELETE_USER"));
-        roleService.createRoleIfNotFound("ROLE_CONTRIBUTOR", Arrays.asList("UPDATE_ROLE","UPDATE_USER"));
-        roleService.createRoleIfNotFound("ROLE_DESIGN", Collections.singletonList("READ_USER"));
+    }
 
-        userService.createInitUser("zaharioudakis@yahoo.com", "Nikolas", "Zacharioudakis",
-                adminUser, password, Collections.singletonList("ROLE_ADMIN"));
-        userService.createInitUser("contributor@yahoo.com", "Vasilis", "Zacharioudakis",
-                contributorUser, password, Collections.singletonList("ROLE_CONTRIBUTOR"));
-        userService.createInitUser("designer@yahoo.com", "Antonis", "Zacharioudakis",
-                designerUser, password, Collections.singletonList("ROLE_DESIGN"));
 
+    private void initPermissions() {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("app/initData/initPermission.json")) {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray permissionList = (JSONArray) obj;
+
+            //Iterate over employee array
+            permissionList.forEach(permission -> parsePermissionObject((JSONObject) permission));
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parsePermissionObject(JSONObject permission) {
+        //Get employee object within list
+        String name = permission.get("name").toString();
+        List<String> depsList = new ArrayList<String>();
+        JSONArray arr = (JSONArray) permission.get("deps");
+        for (Object perObject : arr) {
+            depsList.add(perObject.toString());
+        }
+        permissionService.createPermissionIfNotFound(name, depsList);
+    }
+
+    void initRoles() {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("app/initData/initRoles.json")) {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray permissionList = (JSONArray) obj;
+            //Iterate over employee array
+            permissionList.forEach(role -> parseRoleObject((JSONObject) role));
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseRoleObject(JSONObject permission) {
+        //Get employee object within list
+        String name = permission.get("name").toString();
+        List<String> permissionList = new ArrayList<String>();
+        JSONArray arr = (JSONArray) permission.get("permissionList");
+        for (Object perObject : arr) {
+            permissionList.add(perObject.toString());
+        }
+        roleService.createRoleIfNotFound(name, permissionList);
+    }
+
+    void initUsers() {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("app/initData/initUsers.json")) {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray permissionList = (JSONArray) obj;
+            //Iterate over employee array
+            permissionList.forEach(user -> parseUserObject((JSONObject) user));
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseUserObject(JSONObject permission) {
+        //Get employee object within list
+        String email = permission.get("email").toString();
+        String firstName = permission.get("firstName").toString();
+        String lastName = permission.get("lastName").toString();
+        String username = permission.get("username").toString();
+        String password = permission.get("password").toString();
+        List<String> rolesList = new ArrayList<String>();
+        JSONArray arr = (JSONArray) permission.get("rolesList");
+        for (Object perObject : arr) {
+            rolesList.add(perObject.toString());
+        }
+        userService.createInitUser(email, firstName, lastName, username, password, rolesList);
     }
 }
