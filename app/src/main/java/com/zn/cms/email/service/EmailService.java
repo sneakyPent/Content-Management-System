5f0cae3390cf5d3b6188edc435cf108d9ffca69f
sneakyPent <zaharioudakis@yahoo.com>
2022-10-13 00:11:15 +0300

    added methods in EmailService.java for sending simple mail and mail with attachment

--- app/src/main/java/com/zn/cms/email/service/EmailService.java
+++ app/src/main/java/com/zn/cms/email/service/EmailService.java
@@ -1,10 +1,13 @@
 package com.zn.cms.email.service;
 
-import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import javax.mail.MessagingException;
 
-import java.util.List;
 public interface EmailService {
-    void sendMail(String to, String subject, String body) throws MessagingException;
+
+    void sendSimpleMessage(String to, String subject, String body);
+
+    void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException;
+
     void sendPasswordResetEmail(String email, String subject);
 
 }
