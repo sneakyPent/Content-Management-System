4d82bb4503f1e7ea85a1583bb447d8019be4f4ed
sneakyPent <zaharioudakis@yahoo.com>
2022-10-13 00:12:38 +0300

    implement sendMessageWithAttachment in EmailServiceImpl.java

--- app/src/main/java/com/zn/cms/email/service/EmailServiceImpl.java
+++ app/src/main/java/com/zn/cms/email/service/EmailServiceImpl.java
@@ -46,8 +46,24 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void sendMail(String to, String subject, String body) throws MessagingException {
+    public void sendMessageWithAttachment(
+            String to, String subject, String text, String pathToAttachment) throws MessagingException {
 
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("zacharin4@gmail.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+        FileSystemResource file
+                = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment("Invoice.txt", file);
+
+        emailSender.send(message);
+        // ...
     }
 
     @Override
