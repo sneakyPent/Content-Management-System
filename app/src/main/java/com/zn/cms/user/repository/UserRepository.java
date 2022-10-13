68b6254e7a695f4ac457d06ed9c4c853153be4c1
zn <zaharioudakis@yahoo.com>
2022-10-13 12:29:58 +0300

    update UserRepository (findByEmail, findByResetPasswordToken)

--- app/src/main/java/com/zn/cms/user/repository/UserRepository.java
+++ app/src/main/java/com/zn/cms/user/repository/UserRepository.java
@@ -11,4 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByResetPasswordToken(String token);
+
 }
