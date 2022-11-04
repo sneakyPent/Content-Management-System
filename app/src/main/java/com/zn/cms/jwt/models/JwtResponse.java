2fcf441b0be023d74aa17c55cf55987a7daeb372
zn <zaharioudakis@yahoo.com>
2022-11-04 17:55:02 +0200

    remove @Data annotation from JwtResponse.java

--- app/src/main/java/com/zn/cms/jwt/models/JwtResponse.java
+++ app/src/main/java/com/zn/cms/jwt/models/JwtResponse.java
@@ -5,8 +5,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
-//@AllArgsConstructor
+
 public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
