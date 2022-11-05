baef00fcc22527777fc180ca5d66cebbd92ffb0f
zn <zaharioudakis@yahoo.com>
2022-11-05 17:56:38 +0200

    renaming jwttoken in JwtResponse.java

--- app/src/main/java/com/zn/cms/jwt/models/JwtResponse.java
+++ app/src/main/java/com/zn/cms/jwt/models/JwtResponse.java
@@ -9,12 +9,12 @@ import java.io.Serializable;
 public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
-	private final String jwttoken;
-	public JwtResponse(String jwttoken) {
-		this.jwttoken = jwttoken;
+	private final String jwtToken;
+	public JwtResponse(String jwtToken) {
+		this.jwtToken = jwtToken;
 	}
 
 	public String getToken() {
-		return this.jwttoken;
+		return this.jwtToken;
 	}
 }
