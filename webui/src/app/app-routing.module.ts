96acd6bbb1ba9ff4276966096d914ffadb8115d3
Zacharioudakis <zaha.nikolas@yahoo.com>
2022-11-20 20:09:42 +0200

    add angular

---
+++ webui/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  // { path: 'tutorials', component: TutorialsListComponent },
+  // { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  // { path: 'add', component: AddTutorialComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
